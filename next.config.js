8bf7832f72ffe6dde1c6fd1ecc049ad85a756a9b
raj-y12 <raj.d.yendamuri@gmail.com>
2025-09-14 13:29:28 +0100

    Allow deployment ignoring ESLint errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your config options
+  eslint: {
+    // Ignore ESLint errors during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
